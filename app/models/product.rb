5c9451be8cecc1d7ceef71d31eb1b2f880995552
anfaal <anfaalbhatti71@gmail.com>
2023-08-16 22:52:48 +0500

    delete all the migrations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_one :product_variation_count
+  has_one :variation_style, through: :product_variation_count
+  has_many :purchase_orders_products
+  has_many :purchase_orders, through: :purchase_orders_products
+  has_many :prices
 end
--- db/migrate/20230816080550_remove_columns_from_tables.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromTables < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :product_variation_counts, :variation_styles_id, :integer
-    remove_column :purchase_orders_products, :purchase_orders_id, :integer    
-  end
-end
