2a86494937c5657c7fd56b0d278ea2d3f23ed25a
anfaal <anfaalbhatti71@gmail.com>
2023-08-17 00:04:54 +0500

    create variation styles table with association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_16_185039) do
+ActiveRecord::Schema.define(version: 2023_08_16_190046) do
 
   create_table "catagories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2023_08_16_185039) do
     t.index ["catagory_id"], name: "index_sub_catagories_on_catagory_id"
   end
 
+  create_table "variation_styles", force: :cascade do |t|
+    t.string "type"
+    t.integer "variation_type_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["variation_type_id"], name: "index_variation_styles_on_variation_type_id"
+  end
+
   create_table "variation_types", force: :cascade do |t|
     t.string "type"
     t.integer "sub_catagory_id", null: false
@@ -35,5 +43,6 @@ ActiveRecord::Schema.define(version: 2023_08_16_185039) do
   end
 
   add_foreign_key "sub_catagories", "catagories"
+  add_foreign_key "variation_styles", "variation_types"
   add_foreign_key "variation_types", "sub_catagories"
 end
