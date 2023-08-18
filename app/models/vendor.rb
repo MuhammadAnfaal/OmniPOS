d7b0ed589408871ec5bb5d3f16217ee7326690de
anfaal <anfaalbhatti71@gmail.com>
2023-08-18 11:33:25 +0500

    update email validation in vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,11 +1,11 @@
+# app/models/vendor.rb
 class Vendor < ApplicationRecord
   has_many :purchase_orders
   validates_associated :purchase_orders
 
-  validates :first_name, presence :true ,length: { minimum: 5 }
-  validates :last_name, presence :true ,length: { minimum: 5 }
-  validates :email, uniqueness: true, presence :true
-  validates :address, presence :true, length: {minimum:20}
-  validates :phone_number, presence :true, length: {minimum:12, maximum:12}
-  
+  validates :first_name, presence: true, length: { minimum: 5 }
+  validates :last_name, presence: true, length: { minimum: 5 }
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email format" }
+  validates :address, presence: true, length: { minimum: 20 }
+  validates :phone_number, presence: true, length: { is: 12 }
 end
