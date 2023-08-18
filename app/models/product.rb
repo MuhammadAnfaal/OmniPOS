1f292ba0c5551936f2f00454c2aafd48a609466f
anfaal <anfaalbhatti71@gmail.com>
2023-08-18 15:24:51 +0500

    validation and association on product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
-  has_one :product_variation_count
+  has_many :product_variation_detail
   has_one :variation_style, through: :product_variation_count
   has_many :purchase_orders_products
   has_many :purchase_orders, through: :purchase_orders_products
   has_many :prices
+  validates_associated :product_variation_detail
+  validates_associated :purchase_orders_products
+
+  validates :name, presence: true, uniqueness: true, length: {minimum:20}
+  validates :quantity_in_stock, allow_nil: true
+  validates :description, length: {minimum:25}
+  
 end
